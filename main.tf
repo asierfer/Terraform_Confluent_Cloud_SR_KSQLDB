e44f8cb8df1620856465456619c701ba1418d3d2
Asier Fernández <asierfer@gmail.com>
2023-04-24 09:30:40 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,445 @@
+# Configure the Confluent Provider
+//https://github.com/confluentinc/terraform-provider-confluent/blob/master/examples/configurations/single-event-types-avro-schema/main.tf
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.39.0"
+    }
+  }
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+resource "confluent_environment" "staging" {
+  display_name = "${var.my_prefix}env"
+}
+
+# Stream Governance and Kafka clusters can be in different regions as well as different cloud providers,
+# but you should to place both in the same cloud and region to restrict the fault isolation boundary.
+data "confluent_schema_registry_region" "essentials" {
+  cloud   = "AWS"
+  region  = "us-east-2"
+  package = "ESSENTIALS"
+}
+
+resource "confluent_schema_registry_cluster" "essentials" {
+  package = data.confluent_schema_registry_region.essentials.package
+
+  environment {
+    id = confluent_environment.staging.id
+  }
+
+  region {
+    # See https://docs.confluent.io/cloud/current/stream-governance/packages.html#stream-governance-regions
+    id = data.confluent_schema_registry_region.essentials.id
+  }
+}
+
+# Update the config to use a cloud provider and region of your choice.
+# https://registry.terraform.io/providers/confluentinc/confluent/latest/docs/resources/confluent_kafka_cluster
+resource "confluent_kafka_cluster" "basic" {
+  display_name = "${var.my_prefix}inventory"
+  availability = "SINGLE_ZONE"
+  cloud        = "AWS"
+  region       = "us-east-2"
+  standard {}
+  environment {
+    id = confluent_environment.staging.id
+  }
+}
+
+// 'app-manager' service account is required in this configuration to create 'purchase' topic and grant ACLs
+// to 'app-producer' and 'app-consumer' service accounts.
+resource "confluent_service_account" "app-manager" {
+  display_name = "${var.my_prefix}app-manager"
+  description  = "Service account to manage 'inventory' Kafka cluster"
+}
+
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.basic.rbac_crn
+}
+
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "${var.my_prefix}app-manager-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+
+  # The goal is to ensure that confluent_role_binding.app-manager-kafka-cluster-admin is created before
+  # confluent_api_key.app-manager-kafka-api-key is used to create instances of
+  # confluent_kafka_topic, confluent_kafka_acl resources.
+
+  # 'depends_on' meta-argument is specified in confluent_api_key.app-manager-kafka-api-key to avoid having
+  # multiple copies of this definition in the configuration which would happen if we specify it in
+  # confluent_kafka_topic, confluent_kafka_acl resources instead.
+  depends_on = [
+    confluent_role_binding.app-manager-kafka-cluster-admin
+  ]
+}
+
+resource "confluent_kafka_topic" "purchase" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  topic_name    = "purchase"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_kafka_topic" "visualizaciones" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  topic_name         = "visualizaciones_t"
+  partitions_count   = 3
+  rest_endpoint      = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+
+  //lifecycle {
+    //prevent_destroy = true
+  //}
+}
+
+resource "confluent_connector" "datagen_source_visualizaciones" {
+  environment {
+    id = confluent_environment.staging.id
+  }
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  config_sensitive = {}
+
+  config_nonsensitive = {
+    "connector.class"          = "DatagenSource"
+    "name"                     = "${var.my_prefix}DatagenSourceConnector_vis"
+    "kafka.auth.mode"          = "KAFKA_API_KEY"
+    "kafka.api.key"            = confluent_api_key.app-manager-kafka-api-key.id
+    "kafka.api.secret"         = confluent_api_key.app-manager-kafka-api-key.secret
+    "kafka.topic"              = confluent_kafka_topic.visualizaciones.topic_name
+    "output.data.format"       = "AVRO"
+    "quickstart"               = "PAGEVIEWS"
+    "tasks.max"                = "1"
+  }
+
+/*
+  lifecycle {
+    prevent_destroy = true
+  }
+*/
+}
+
+resource "confluent_kafka_topic" "usuarios" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  topic_name         = "usuarios_visual_t"
+  partitions_count   = 3
+  rest_endpoint      = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+
+  //lifecycle {
+    //prevent_destroy = true
+  //}
+}
+
+resource "confluent_connector" "datagen_source_usuarios" {
+  environment {
+    id = confluent_environment.staging.id
+  }
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+
+  config_sensitive = {}
+
+  config_nonsensitive = {
+    "connector.class"          = "DatagenSource"
+    "name"                     = "${var.my_prefix}DatagenSourceConnector_us"
+    "kafka.auth.mode"          = "KAFKA_API_KEY"
+    "kafka.api.key"            = confluent_api_key.app-manager-kafka-api-key.id
+    "kafka.api.secret"         = confluent_api_key.app-manager-kafka-api-key.secret
+    "kafka.topic"              = confluent_kafka_topic.usuarios.topic_name
+    "output.data.format"       = "AVRO"
+    "quickstart"               = "USERS"
+    "tasks.max"                = "1"
+  }
+
+/*
+  lifecycle {
+    prevent_destroy = true
+  }
+*/
+}
+
+resource "confluent_service_account" "app-consumer" {
+  display_name = "${var.my_prefix}app-consumer"
+  description  = "Service account to consume from 'purchase' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_api_key" "app-consumer-kafka-api-key" {
+  display_name = "${var.my_prefix}app-consumer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-consumer' service account"
+  owner {
+    id          = confluent_service_account.app-consumer.id
+    api_version = confluent_service_account.app-consumer.api_version
+    kind        = confluent_service_account.app-consumer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+}
+
+resource "confluent_kafka_acl" "app-producer-write-on-topic" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "TOPIC"
+  resource_name = confluent_kafka_topic.purchase.topic_name
+  pattern_type  = "LITERAL"
+  principal     = "User:${confluent_service_account.app-producer.id}"
+  host          = "*"
+  operation     = "WRITE"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_service_account" "app-producer" {
+  display_name = "${var.my_prefix}app-producer"
+  description  = "Service account to produce to 'purchase' topic of 'inventory' Kafka cluster"
+}
+
+resource "confluent_api_key" "app-producer-kafka-api-key" {
+  display_name = "${var.my_prefix}app-producer-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-producer' service account"
+  owner {
+    id          = confluent_service_account.app-producer.id
+    api_version = confluent_service_account.app-producer.api_version
+    kind        = confluent_service_account.app-producer.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+}
+
+// Note that in order to consume from a topic, the principal of the consumer ('app-consumer' service account)
+// needs to be authorized to perform 'READ' operation on both Topic and Group resources:
+// confluent_kafka_acl.app-consumer-read-on-topic, confluent_kafka_acl.app-consumer-read-on-group.
+// https://docs.confluent.io/platform/current/kafka/authorization.html#using-acls
+resource "confluent_kafka_acl" "app-consumer-read-on-topic" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "TOPIC"
+  resource_name = confluent_kafka_topic.purchase.topic_name
+  pattern_type  = "LITERAL"
+  principal     = "User:${confluent_service_account.app-consumer.id}"
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_kafka_acl" "app-consumer-read-on-group" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  resource_type = "GROUP"
+  // The existing values of resource_name, pattern_type attributes are set up to match Confluent CLI's default consumer group ID ("confluent_cli_consumer_<uuid>").
+  // https://docs.confluent.io/confluent-cli/current/command-reference/kafka/topic/confluent_kafka_topic_consume.html
+  // Update the values of resource_name, pattern_type attributes to match your target consumer group ID.
+  // https://docs.confluent.io/platform/current/kafka/authorization.html#prefixed-acls
+  resource_name = "confluent_cli_consumer_"
+  pattern_type  = "PREFIXED"
+  principal     = "User:${confluent_service_account.app-consumer.id}"
+  host          = "*"
+  operation     = "READ"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key    = confluent_api_key.app-manager-kafka-api-key.id
+    secret = confluent_api_key.app-manager-kafka-api-key.secret
+  }
+}
+
+resource "confluent_service_account" "env-manager" {
+  display_name = "${var.my_prefix}env-manager"
+  description  = "Service account to manage 'Staging' environment"
+}
+
+resource "confluent_role_binding" "env-manager-environment-admin" {
+  principal   = "User:${confluent_service_account.env-manager.id}"
+  role_name   = "EnvironmentAdmin"
+  crn_pattern = confluent_environment.staging.resource_name
+}
+
+resource "confluent_api_key" "env-manager-schema-registry-api-key" {
+  display_name = "${var.my_prefix}env-manager-schema-registry-api-key"
+  description  = "Schema Registry API Key that is owned by 'env-manager' service account"
+  owner {
+    id          = confluent_service_account.env-manager.id
+    api_version = confluent_service_account.env-manager.api_version
+    kind        = confluent_service_account.env-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_schema_registry_cluster.essentials.id
+    api_version = confluent_schema_registry_cluster.essentials.api_version
+    kind        = confluent_schema_registry_cluster.essentials.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+
+  # The goal is to ensure that confluent_role_binding.env-manager-environment-admin is created before
+  # confluent_api_key.env-manager-schema-registry-api-key is used to create instances of
+  # confluent_schema resources.
+
+  # 'depends_on' meta-argument is specified in confluent_api_key.env-manager-schema-registry-api-key to avoid having
+  # multiple copies of this definition in the configuration which would happen if we specify it in
+  # confluent_schema resources instead.
+  depends_on = [
+    confluent_role_binding.env-manager-environment-admin
+  ]
+}
+
+resource "confluent_schema" "purchase" {
+  schema_registry_cluster {
+    id = confluent_schema_registry_cluster.essentials.id
+  }
+  rest_endpoint = confluent_schema_registry_cluster.essentials.rest_endpoint
+  subject_name = "purchase-value"
+  format = "AVRO"
+  schema = file("./user-purchase.avsc")
+  credentials {
+    key    = confluent_api_key.env-manager-schema-registry-api-key.id
+    secret = confluent_api_key.env-manager-schema-registry-api-key.secret
+  }
+}
+
+// ksqlDB service account
+resource "confluent_service_account" "app-ksql" {
+  display_name = "${var.my_prefix}app-ksql"
+  description  = "Service account for ksqlDB cluster"
+}
+
+resource "confluent_role_binding" "app-ksql-all-topic" {
+  principal   = "User:${confluent_service_account.app-ksql.id}"
+  role_name   = "ResourceOwner"
+  crn_pattern = "${confluent_kafka_cluster.basic.rbac_crn}/kafka=${confluent_kafka_cluster.basic.id}/topic=*"
+}
+
+resource "confluent_role_binding" "app-ksql-all-group" {
+  principal   = "User:${confluent_service_account.app-ksql.id}"
+  role_name   = "ResourceOwner"
+  crn_pattern = "${confluent_kafka_cluster.basic.rbac_crn}/kafka=${confluent_kafka_cluster.basic.id}/group=*"
+}
+
+resource "confluent_role_binding" "app-ksql-all-transactions" {
+  principal   = "User:${confluent_service_account.app-ksql.id}"
+  role_name   = "ResourceOwner"
+  crn_pattern = "${confluent_kafka_cluster.basic.rbac_crn}/kafka=${confluent_kafka_cluster.basic.id}/transactional-id=*"
+}
+
+# ResourceOwner roles above are for KSQL service account to read/write data from/to kafka,
+# this role instead is needed for giving access to the Ksql cluster.
+resource "confluent_role_binding" "app-ksql-ksql-admin" {
+  principal   = "User:${confluent_service_account.app-ksql.id}"
+  role_name   = "KsqlAdmin"
+  crn_pattern = confluent_ksql_cluster.main.resource_name
+}
+
+resource "confluent_role_binding" "app-ksql-schema-registry-resource-owner" {
+  principal   = "User:${confluent_service_account.app-ksql.id}"
+  role_name   = "ResourceOwner"
+  crn_pattern = format("%s/%s", confluent_schema_registry_cluster.essentials.resource_name, "subject=*")
+}
+
+resource "confluent_ksql_cluster" "main" {
+  display_name = "ksql_cluster_0"
+  csu = 1
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  credential_identity {
+    id = confluent_service_account.app-ksql.id
+  }
+  environment {
+    id = confluent_environment.staging.id
+  }
+  depends_on = [
+    confluent_role_binding.app-ksql-schema-registry-resource-owner,
+    confluent_schema_registry_cluster.essentials
+  ]
+}
+
+resource "confluent_api_key" "app-ksqldb-api-key" {
+  display_name = "app-ksqldb-api-key"
+  description  = "KsqlDB API Key that is owned by 'app-ksql' service account"
+  owner {
+    id          = confluent_service_account.app-ksql.id
+    api_version = confluent_service_account.app-ksql.api_version
+    kind        = confluent_service_account.app-ksql.kind
+  }
+
+  managed_resource {
+    id          = confluent_ksql_cluster.main.id
+    api_version = confluent_ksql_cluster.main.api_version
+    kind        = confluent_ksql_cluster.main.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
+}
